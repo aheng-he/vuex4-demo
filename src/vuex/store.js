89a074efae47e4930e1c258cc3c5983e5cc246fd
hhhhhhqqqqqq <38716155+hhhhhhqqqqqq@users.noreply.github.com>
2021-09-12 23:00:07 +0800

    feat:初始化getter，actions,mutations

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,6 +1,10 @@
 import { storeKey } from './injectKey';
 import ModuleCollection from './module/module-collection';
+import { isPromise } from './utils';
 
+function getNestedState(state, path) { // 根据路径 获取store.上面的最新状态
+    return path.reduce((state, key) => state[key], state)
+}
 
 function installModule(store, rootState, path, module) { // 递归安装
     let isRoot = !path.length;  // 如果数组是空数组 说明是根，否则不是
@@ -25,6 +29,33 @@ function installModule(store, rootState, path, module) { // 递归安装
         installModule(store, rootState, path.concat(key), child)
     })
 
+    module.forEachGetter((getter, key)=>{  // {double: function(state){}}
+        store._wrappedGetters[key] = ()=>{
+            // store.state 使用store.state是因为module.state不是响应式的
+            return getter(getNestedState(store.state, path))
+        }
+    })
+    // mutation   {add:[mutation]}
+    module.forEachMutation((mutation, key)=>{
+        const entry = store._mutations[key] || (store._mutations[key] = [])
+        entry.push((payload)=>{  // store.commit("add", payload)
+            mutation.call(store, getNestedState(store.state, path), payload)
+        })
+    })
+
+    // actions mutation和action的一个区别， action执行后返回一个是promise 
+    module.forEachAction((action, key)=>{
+        const entry = store._actions[key] || (store._actions[key] = [])
+        entry.push((payload)=>{
+            let res = action.call(store, store, payload)
+            // 判断res是否为一个promise
+            if(!isPromise(res)){
+                return Promise.resolve(res)
+            }
+            return res
+        })
+    })
+
 }
 // 创建容器  返回store
 export default class Store {
@@ -33,11 +64,17 @@ export default class Store {
         const store = this;
         // 收集，模块
         store._modules = new ModuleCollection(options)
-
+        
+        // {add:[fn,fn,fn]}  发布订阅模式
+        store._wrappedGetters = Object.create(null);
+        store._mutations = Object.create(null);
+        store._actions = Object.create(null);
         // 定义状态
         const state = store._modules.root.state; // 根状态
 
         installModule(store, state, [], store._modules.root);
+
+       
 
     }
 
